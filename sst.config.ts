37d99f34a542490e4d74d09f98724893ec009536
Yuvraj Singh <yuvraj.mark3@gmail.com>
2025-04-05 22:28:07 +0530

    changed sst config for env error

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,5 @@
-import type { SSTConfig } from "sst";
-import {Config, NextjsSite } from "sst/constructs";
+import { type SSTConfig } from "sst";
+import { NextjsSite } from "sst/constructs";
 
 export default {
   config(_input) {
@@ -10,18 +10,19 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const SECRET_KEY = new Config.Secret(stack, "SECRET_KEY");
-      const AUTH_SECRET = new Config.Secret(stack, "AUTH_SECRET");
-      const AUTH_GOOGLE_ID = new Config.Secret(stack, "AUTH_GOOGLE_ID");
-      const AUTH_GOOGLE_SECRET = new Config.Secret(stack, "AUTH_GOOGLE_SECRET");
-      const NEXT_PUBLIC_SUPABASE_URL = new Config.Secret(stack, "NEXT_PUBLIC_SUPABASE_URL");
-      const NEXT_PUBLIC_SUPABASE_ANON_KEY = new Config.Secret(stack, "NEXT_PUBLIC_SUPABASE_ANON_KEY");
-      const DATABASE_URL = new Config.Secret(stack, "DATABASE_URL");
-      const DIRECT_URL = new Config.Secret(stack, "DIRECT_URL");
-      const NEXTAUTH_URL = new Config.Secret(stack, "NEXTAUTH_URL");
       const site = new NextjsSite(stack, "NextSite", {
         path: ".",
-        bind: [SECRET_KEY, AUTH_SECRET, AUTH_GOOGLE_ID, AUTH_GOOGLE_SECRET, NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY, DATABASE_URL, DIRECT_URL, NEXTAUTH_URL],
+        environment: {
+          SECRET_VAL: process.env.SECRET_VAL!,
+          AUTH_SECRET: process.env.AUTH_SECRET!,
+          AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID!,
+          AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET!,
+          DATABASE_URL: process.env.DATABASE_URL!,
+          DIRECT_URL: process.env.DIRECT_URL!,
+          NEXTAUTH_URL: process.env.NEXTAUTH_URL!,
+          NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+          NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        },
       });
 
       stack.addOutputs({
